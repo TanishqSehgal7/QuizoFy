ded1910b7471de2ba5b16fff632994387711dd20
TanishqSehgal7 <tsgl7246@gmail.com>
2020-04-13 18:21:33 +0530

    Firebase Cloud Messaging established

--- app/src/main/java/com/example/quizofy/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/quizofy/MyFirebaseMessagingService.kt
@@ -1,6 +1,16 @@
 package com.example.quizofy
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_ONE_SHOT
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationBuilderWithBuilderAccessor
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -22,7 +32,30 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
 
        remoteMessage?.notification.let {
+           sendNotification(remoteMessage.notification?.body)
        }
     }
 
+    private fun sendNotification(messageBody:String?){
+        val intent=Intent(this,MainActivity::class.java)
+        val addFlags = intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT)
+        val channelId=getString(R.string.notification_channel_description)
+        val defaultSoundUri=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder=NotificationCompat.Builder(this,channelId)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(getString(R.string.fcm_fallback_notification_channel_label))
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.O){
+            val channel=NotificationChannel(channelId,"Channel title",NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0,notificationBuilder.build())
+    }
 }
