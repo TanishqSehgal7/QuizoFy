74c46598a5cbc698a9cfbd83a70d1f644cfe3a26
TanishqSehgal7 <tsgl7246@gmail.com>
2020-05-23 04:39:55 +0530

    Feature added: In App Mail to Developer made functional

--- app/src/main/java/com/example/quizofy/AboutAppFragment.kt
+++ app/src/main/java/com/example/quizofy/AboutAppFragment.kt
@@ -1,16 +1,18 @@
 package com.example.quizofy
 
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.fragment_about_app.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +20,7 @@ import kotlinx.android.synthetic.main.fragment_about_app.*
 class AboutAppFragment : Fragment() {
     lateinit var bottomnav: BottomNavigationView
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,10 +32,23 @@ class AboutAppFragment : Fragment() {
         val iv = view.findViewById<ImageView>(R.id.iv)
         iv.bringToFront()
         iv.setImageResource(R.drawable.quesmrk)
+
+        val btnSend=view.findViewById<Button>(R.id.btnEmail)
+        btnSend.setOnClickListener {
+            val emailIntent = Intent(
+                Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", "tsgl7246@gmail.com", null))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Body")
+            startActivity(Intent.createChooser(emailIntent, "Send email..."))
+        }
+
+
         return view
     }
     override fun onResume() {
         super.onResume()
         bottomnav.menu.findItem(R.id.aboutApp).isChecked = true
     }
+
 }
